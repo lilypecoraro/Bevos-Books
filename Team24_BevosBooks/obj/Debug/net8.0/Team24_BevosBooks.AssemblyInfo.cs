44e5ba48a90bb53ef3f45d543eb63501272bea62
lilypecoraro <lilypecoraro@utexas.edu>
2025-11-17 10:46:30 -0600

    - Coupons, OrderDetails, Orders model classes

--- Team24_BevosBooks/obj/Debug/net8.0/Team24_BevosBooks.AssemblyInfo.cs
+++ Team24_BevosBooks/obj/Debug/net8.0/Team24_BevosBooks.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team24_BevosBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d9d0ec8a33350e63a06103c4168d07e6e25497")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a605f022c0af98db212229d0c674cae5b4709d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team24_BevosBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team24_BevosBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
