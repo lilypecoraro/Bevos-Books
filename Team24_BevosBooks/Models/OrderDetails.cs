95ffd70aeb952e8eec3f4111c5d9aa8d760b20b2
Sachi <173219042+sachi-sooda@users.noreply.github.com>
2025-11-18 12:59:12 -0600

    Update OrderDetails.cs

--- Team24_BevosBooks/Models/OrderDetails.cs
+++ Team24_BevosBooks/Models/OrderDetails.cs
@@ -1,11 +1,38 @@
-﻿using System;
-namespace Team24_BevosBooks.Models
-{
-	public class OrderDetails
-	{
-		public OrderDetails()
-		{
-		}
-	}
-}
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BevosBooks.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailsID { get; set; }   // PK
+
+        [ForeignKey("Order")]
+        public int OrderID { get; set; }          // FK to Order
+
+        [ForeignKey("Book")]
+        public int BookID { get; set; }           // FK to Book
+
+        [ForeignKey("Card")]
+        public int CardID { get; set; }           // FK to Card used for payment
+
+        [ForeignKey("Coupon")]
+        public int? CouponID { get; set; }        // Optional FK to Coupon
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required, Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }        // Selling price at time of order
+
+        [Required, Column(TypeName = "decimal(18,2)")]
+        public decimal Cost { get; set; }         // Supplier cost at time of order
+
+        // Navigation properties
+        public Order Order { get; set; }
+        public Book Book { get; set; }
+        public Card Card { get; set; }
+        public Coupon Coupon { get; set; }
+    }
+}
