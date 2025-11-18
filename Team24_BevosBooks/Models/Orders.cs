312dc83af3830167a72946fd57f0e332502e63b7
Sachi <173219042+sachi-sooda@users.noreply.github.com>
2025-11-18 13:00:38 -0600

    Update Orders.cs

--- Team24_BevosBooks/Models/Orders.cs
+++ Team24_BevosBooks/Models/Orders.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BevosBooks.Models
+namespace Team24_BevosBooks.Models
 {
     public class Order
     {
