d9013f4148d0fe33dad8cec71f4d36657e09833e
Sachi <173219042+sachi-sooda@users.noreply.github.com>
2025-11-18 12:59:51 -0600

    Update Books.cs

--- Team24_BevosBooks/Models/Books.cs
+++ Team24_BevosBooks/Models/Books.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BevosBooks.Models
+namespace Team24_BevosBooks.Models
 {
     public class Book
     {
