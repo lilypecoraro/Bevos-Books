9d61e5fbbb9bb0695bc19f934588183c99467d29
Sachi <173219042+sachi-sooda@users.noreply.github.com>
2025-11-18 12:59:00 -0600

    Update Orders.cs

--- Team24_BevosBooks/Models/Orders.cs
+++ Team24_BevosBooks/Models/Orders.cs
@@ -1,11 +1,30 @@
-﻿using System;
-namespace Team24_BevosBooks.Models
-{
-	public class Orders
-	{
-		public Orders()
-		{
-		}
-	}
-}
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BevosBooks.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }   // PK
+
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }  // FK to User (Customer)
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ShippingFee { get; set; }
+
+        [Required, StringLength(50)]
+        public string OrderStatus { get; set; }  // e.g., Placed, Shipped, Cancelled
+
+        // Navigation properties
+        public User Customer { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
