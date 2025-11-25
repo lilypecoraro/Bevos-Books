b8bbfbc3b1c6f9fece7b7dc997b64b66c59d9a51
Skyler-Nguyen <skylernguyen0716@gmail.com>
2025-11-24 23:49:31 -0600

    homecontroller, home/index, shared views

--- Team24_BevosBooks/Controllers/HomeController.cs
+++ Team24_BevosBooks/Controllers/HomeController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Team24_BevosBooks.DAL;
+using Team24_BevosBooks.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Team24_BevosBooks.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // ----------------------------
+        // LANDING PAGE (Requirements: welcome, promotions, search bar)
+        // ----------------------------
+        public async Task<IActionResult> Index(string? searchString)
+        {
+            IQueryable<Book> query = _context.Books;
+
+            // optional search
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                query = query.Where(b =>
+                    b.Title.Contains(searchString) ||
+                    b.Author.Contains(searchString));
+            }
+
+            // Home page shows 6 books max (not required, but clean)
+            var books = await query
+                .OrderBy(b => b.Title)
+                .Take(6)
+                .ToListAsync();
+
+            return View(books);
         }
     }
 }
