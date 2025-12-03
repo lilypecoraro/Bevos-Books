c0934c0ca452d346242d991ff0e0e47ecda2b3dd
lilypecoraro <lilypecoraro@utexas.edu>
2025-12-03 14:23:22 -0600

    Fixed cart view.

--- Team24_BevosBooks/obj/Debug/net8.0/Team24_BevosBooks.AssemblyInfo.cs
+++ Team24_BevosBooks/obj/Debug/net8.0/Team24_BevosBooks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Team24_BevosBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932daf6ed7be43d9c61a17ab21af7343f32d6333")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0499910cffb9ab26e4ae20a3aac9ca5c95c8c7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Team24_BevosBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Team24_BevosBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
